fb4a1056abb23500ceb3c60ea380a9b8bcbd0231
vilho4 <ville.j.holopainen@student.jyu.fi>
2026-09-16 15:55:46 +0300

    14.14 completed

--- bloglist-app/app/actions/users.ts
+++ bloglist-app/app/actions/users.ts
@@ -4,11 +4,47 @@ import { redirect } from 'next/navigation'
 import bcrypt from 'bcryptjs'
 import { db } from '../../db'
 import { users } from '../../db/schema'
+import { eq } from 'drizzle-orm'
 
-export const registerUser = async (formData: FormData) => {
+export const registerUser = async (previousstate: unknown, formData: FormData) => {
   const username = (formData.get('username') as string)?.trim()
   const name = (formData.get('name') as string)?.trim()
   const password = formData.get('password') as string
+  const passwordConfirm = formData.get('passwordConfirm') as string
+
+  console.log(password, 'password test')
+  console.log(passwordConfirm, 'password confirmation test')
+
+  const errors: {
+    username?: string
+    name?: string
+    password?: string
+    passwordConfirm?: string
+  } = {}
+
+  if (username.length < 4) {
+    errors.username = 'Username must be at least 4 characters long'
+  } else {
+    const existingUser = await db.query.users.findFirst({
+      where: eq(users.username, username),
+    })
+
+    if (existingUser) {
+      errors.username = 'Username is already taken'
+    }
+  }
+
+  if (password.length < 4) {
+    errors.password = 'Password must be at least 4 characters long'
+  }
+
+  if (password !== passwordConfirm) {
+    errors.passwordConfirm = 'Passwords do not match'
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors, values: { username, name, password, passwordConfirm: passwordConfirm } }
+  }
 
   const passwordHash = await bcrypt.hash(password, 10)
 
--- bloglist-app/app/register/page.tsx
+++ bloglist-app/app/register/page.tsx
@@ -1,23 +1,53 @@
+'use client'
 import Link from 'next/link'
 import { registerUser } from '../actions/users'
+import { useActionState } from 'react'
+
+type FormState = {
+  errors: {
+    username?: string
+    name?: string
+    password?: string
+    passwordConfirm?: string
+  }
+  values: {
+    username: string
+    name: string
+    password: string
+    passwordConfirm: string
+  }
+}
+
+const initialState: FormState = {
+  errors: {},
+  values: {
+    username: '',
+    name: '',
+    password: '',
+    passwordConfirm: '',
+  },
+}
 
 export default function RegisterPage() {
+  const [state, formAction] = useActionState(registerUser, initialState)
   return (
     <div>
       <h2>Register</h2>
 
-      <form action={registerUser}>
+      <form action={formAction}>
         <div>
           <label>
             Username
-            <input type="text" name="username" required />
+            <input type="text" name="username" defaultValue={state.values.username} required />
           </label>
+          {state.errors.username && <div style={{ color: 'red' }}>{state.errors.username}</div>}
         </div>
         <div>
           <label>
             Name
-            <input type="text" name="name" required />
+            <input type="text" name="name" defaultValue={state.values.name} required />
           </label>
+          {state.errors.name && <div style={{ color: 'red' }}>{state.errors.name}</div>}
         </div>
 
         <div>
@@ -25,6 +55,16 @@ export default function RegisterPage() {
             Password
             <input type="password" name="password" required />
           </label>
+          {state.errors.password && <div style={{ color: 'red' }}>{state.errors.password}</div>}
+        </div>
+        <div>
+          <label>
+            Password Confirmation
+            <input type="password" name="passwordConfirm" required />
+          </label>
+          {state.errors.passwordConfirm && (
+            <div style={{ color: 'red' }}>{state.errors.passwordConfirm}</div>
+          )}
         </div>
 
         <button type="submit">Register</button>
